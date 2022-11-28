3cb6768a69531985e3a60589ef424d51e1fb29bc
Broken Helmet <administrator@brokenhelmet.co.ke>
2022-11-28 03:22:50 +0300

    Addition of AsyncHelper class to test Task.Yield() loop solution

---
+++ EosSharp/EosSharp.Unity3D/AsyncHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Assets.Packages.eossharp.EosSharp.EosSharp.Unity3D
+{
+
+
+    public static class AsyncHelper
+    {
+        public static int timer = 0;
+
+        public static async Task Delay(int duration)
+        {
+#if UNITY_WEBGL
+            //await UniTask.Delay(duration); 
+            Debug.Log($"Custom delay has been called for duration {duration}");
+            
+            while(timer < duration)
+            {
+                await Task.Yield();
+                timer++;
+            }
+
+            timer = 0;
+#else
+            await Task.Delay(duration);
+
+#endif
+        }
+    }
+}
