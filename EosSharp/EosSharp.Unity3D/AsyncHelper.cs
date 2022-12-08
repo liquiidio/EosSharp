24b01dd7904f9523a6380ff71fc84dfa321f72a3
cmadh <grizzlycorv@gmail.com>
2022-12-09 00:17:15 +0100

    cleaning up, removing code-debt

--- EosSharp/EosSharp.Unity3D/AsyncHelper.cs
+++ EosSharp/EosSharp.Unity3D/AsyncHelper.cs
@@ -1,8 +1,4 @@
-﻿#if UNITY_WEBGL
-// ReSharper disable once RedundantUsingDirective
-using Cysharp.Threading.Tasks;
-#endif
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -15,34 +11,14 @@ namespace Assets.Packages.eossharp.EosSharp.EosSharp.Unity3D
             var endTime = DateTimeOffset.Now.AddMilliseconds(durationMiliseconds);
 
 #if UNITY_WEBGL && !UNITY_EDITOR_WIN
-            //await UniTask.Delay(duration); 
-            //Debug.Log($"Custom delay has been called for duration {duration}");
-            int counter = 0;
             while(DateTimeOffset.Now < endTime)
             {
                 await Task.Yield();
-                counter++;
-                Debug.Log($"counter {counter}");
             }
 #else
             await Task.Delay(durationMiliseconds);
 
 #endif
         }
-
-//#if UNITY_WEBGL && !UNITY_EDITOR_WIN
-//            //await UniTask.Delay(duration); 
-//            //Debug.Log($"Custom delay has been called for duration {duration}");
-//            int counter = 0;
-//            while(Time.time < endTime)
-//            {
-//                await Task.Yield();
-//                counter++;
-//                Debug.Log($"counter {counter}");
-//                Debug.Log($"Time.time {Time.time}");
-//            }
-//#else
-//        await Task.Delay(durationMiliseconds);
-//#endif
     }
 }
